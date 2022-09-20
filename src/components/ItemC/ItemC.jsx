560e84dc3a552ceff60b11a42851eb71fc27913a
niederhausertomas <78279996+niederhausertomas@users.noreply.github.com>
2022-09-20 19:47:23 -0300

    arreglo del css del boton de ItemCountl

--- src/components/ItemC/ItemC.jsx
+++ src/components/ItemC/ItemC.jsx
@@ -16,6 +16,7 @@ const ItemC = ({prod})=>{
                 <div className="card-body">
                     <h5 className="card-title">{prod.nombre}</h5>
                     <p className="card-text"> $ {prod.precio}</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  repellendus dolorum fuga totam voluptate delectus dolore deleniti.</p>   
                     <hr/>
                     <ItemCount stock = {5} initial = {1} onAdd = {onAdd} />
                 </div>
