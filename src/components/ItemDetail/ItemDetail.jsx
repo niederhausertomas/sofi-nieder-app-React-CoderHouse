9c2f0bbbc5ca3bf5a4cb74057a7b3be1e1a01675
niederhausertomas <78279996+niederhausertomas@users.noreply.github.com>
2022-09-20 20:23:04 -0300

    estetica del ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -7,7 +7,7 @@ const ItemDetail = ({producto})=>{
         console.log(`se agrego ${cantidad} `)
     }
     return (   
-        <div className="card col-6 col-sm-3">
+     /*   <div className="cardo">
             <img src={producto.imagen} className="card-img-top" alt={producto.nombre}/>
             <div className="card-body">
                 <h5 className="card-title">{producto.nombre}</h5>
@@ -15,7 +15,25 @@ const ItemDetail = ({producto})=>{
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  repellendus dolorum fuga totam voluptate delectus dolore deleniti.</p>
                 <ItemCount stock = {5} initial = {1} onAdd = {onAdd} />
             </div>
+        </div>*/
+
+        <div className="cardo mb-3" >
+            <div className="row g-0">
+                <div className="imagenn col-md-4">
+                    <img src={producto.imagen} className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                    <h5 className="tituloCard">{producto.nombre}</h5>
+                    <p className="tituloCard"> Precio $ {producto.precio}</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  repellendus dolorum fuga totam voluptate delectus dolore deleniti.<br/>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.  repellendus dolorum fuga totam voluptate delectus dolore deleniti.</p>
+                    <ItemCount stock = {5} initial = {1} onAdd = {onAdd} />
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
